f8e255db378b0c92fe9e245df6f549655ef225e3
PannnTastic <pannntastic@gmail.com>
2025-10-31 03:21:38 +0700

    Membuat state variable yang dapat berubah dalam Composable Menyimpan nilai teks yang diinput user (dalam hal ini nama) Mempertahankan nilai saat recomposition terjadi menggunakan remember Menggunakan property delegation (by) untuk akses langsung ke nilai state

--- app/src/main/java/com/example/questuserinput_021/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_021/TeksEdit.kt
@@ -1,9 +1,15 @@
 package com.example.questuserinput_021
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
 fun FormDataDiri(modifier: Modifier){
-
+    var textNama by remember { mutableStateOf(value = "") }
+    var textAlamat by remember { mutableStateOf(value = "") }
+    var textJK by remember { mutableStateOf(value = "") }
 }
