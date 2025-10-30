1a6272f33fc58cf88809103951ef002076f52857
PannnTastic <pannntastic@gmail.com>
2025-10-31 03:26:18 +0700

    Membuat daftar pilihan jenis kelamin yang akan ditampilkan dalam komponen pemilihan (seperti RadioButton atau DropdownMenu).

--- app/src/main/java/com/example/questuserinput_021/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_021/TeksEdit.kt
@@ -16,4 +16,6 @@ fun FormDataDiri(modifier: Modifier){
     var nama by remember { mutableStateOf(value = "") }
     var alamat by remember { mutableStateOf(value = "") }
     var jk by remember { mutableStateOf(value = "") }
+
+    val gender: List<String> = listOf("Laki-laki", "Perempuan")
 }
