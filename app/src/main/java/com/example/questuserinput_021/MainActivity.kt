26737a5a787eeaa1473e8266c5e0858d9d0b1fba
PannnTastic <pannntastic@gmail.com>
2025-11-01 20:15:19 +0700

    memamnggil di main Activity

--- app/src/main/java/com/example/questuserinput_021/MainActivity.kt
+++ app/src/main/java/com/example/questuserinput_021/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestUserInput_021Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    FormDataDiri(
+                    FormRegistrasi(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
